17602f2010e515b738442675155d3935b160f33d
wtavares661-glitch <w.tavares661@gmail.com>
2026-09-12 12:43:02 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+            theme: {
+                extend: {
+                    fontFamily: {
+                        sans: ['Inter', 'sans-serif'],
+                    },
+                    colors: {
+                        primary: '#f97316', // Laranja
+                        primaryHover: '#ea580c', // Laranja hover
+                        secondary: '#fb923c', // Laranja claro
+                        darkBg: '#0b0c16', // Fundo bem escuro
+                        cardBg: '#1a163a', // Roxo escuro pros cards
+                        cardBorder: '#2d255f', // Borda roxa
+                    },
+                    backgroundImage: {
+                        'main-gradient': 'linear-gradient(to bottom, #121528, #161935, #1b1642, #1e1544)',
+                    }
+                }
+            }
+        }
